a0bf9955b52f11381375ea2832cf735d73bd6b82
Jakob Borg <jakob@kastelo.net>
2022-12-24 16:18:41 +0100

    Add more detection mechanisms

--- incontainer_linux.go
+++ incontainer_linux.go
@@ -3,14 +3,23 @@ package incontainer
 import (
 	"bytes"
 	"os"
+	"syscall"
 )
 
 func Detect() bool {
-	return hasDockerEnv() || hasDockerCgroup() || pid1EnvHasContainer()
+	return hasContainerEnv() || isCgroupV2Container() || hasDockerCgroup() || pid1EnvHasContainer() || syscall.Getpid() == 1 || isOpenVZ() || isWSL()
 }
 
-func hasDockerEnv() bool {
-	_, err := os.Stat("/.dockerenv")
+func hasContainerEnv() bool {
+	return exists("/.dockerenv") || exists("/run/.containerenv")
+}
+
+func isOpenVZ() bool {
+	return exists("/proc/vz") && !exists("/proc/bc")
+}
+
+func exists(file string) bool {
+	_, err := os.Stat(file)
 	return err == nil
 }
 
@@ -22,10 +31,31 @@ func hasDockerCgroup() bool {
 	return bytes.Contains(cgroup, []byte("/docker/"))
 }
 
+func isCgroupV2Container() bool {
+	if !exists("/sys/fs/cgroup/cgroup.events") {
+		return false
+	}
+	if exists("/sys/fs/cgroup/cgroup.type") {
+		return true
+	}
+	if !exists("/sys/kernel/cgroup/features") {
+		return true
+	}
+	return false
+}
+
 func pid1EnvHasContainer() bool {
 	bs, err := os.ReadFile("/proc/1/environ")
 	if err != nil {
 		return false
 	}
 	return bytes.Contains(bs, []byte("CONTAINER="))
+}
+
+func isWSL() bool {
+	bs, err := os.ReadFile("/proc/sys/kernel/osrelease")
+	if err != nil {
+		return false
+	}
+	return bytes.Contains(bs, []byte("Microsoft"))
 }
